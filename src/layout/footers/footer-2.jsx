33b5679cb84fc238db9422bacb29bb5bbe546f7c
Ephraim Elakhe <elakhe.ephraim.9@gmail.com>
2026-06-02 21:55:01 +0100

    update

--- src/layout/footers/footer-2.jsx
+++ src/layout/footers/footer-2.jsx
@@ -35,7 +35,7 @@ const FooterTwo = () => {
                             </span>
                           </div>
                           <div className="tp-footer-contact-content">
-                            <p><a href="mailto:shofy@support.com">shofy@mail.com</a></p>
+                            <p><a href="mailto:support@norexfashion.com">support@norexfashion.com</a></p>
                           </div>
                         </div>
                         <div className="tp-footer-contact-item d-flex align-items-start">
